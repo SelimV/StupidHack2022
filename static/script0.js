d3b0ba078d1db198236d203cc00ca6fa6d472ba1
SelimV <selim.virtanen@gmail.com>
2022-05-14 16:01:34 +0300

    vaa

---
+++ static/script0.js
@@ -0,0 +1,45 @@
+function eat() {
+    console.log("Pressed eat")
+    $.ajax({
+        url: "/eat"
+    })
+    console.log("Ran ajax")
+}
+
+var happiness=100;
+var ateRecently=false;
+
+setInterval(
+    function(){
+        happiness-=10;
+        if(happiness<=0){
+            document.getElementById('furycat').style.display='inline';
+            document.getElementById('sadcat').style.display='none';
+            document.getElementById('happycat').style.display='none';
+            document.getElementById('happiestcat').style.display='none';
+            if(!ateRecently){
+                eat();
+                ateRecently=true;
+            }
+            happiness=0
+        }else if(happiness<=40){
+            document.getElementById('furycat').style.display='none';
+            document.getElementById('sadcat').style.display='inline';
+            document.getElementById('happycat').style.display='none';
+            document.getElementById('happiestcat').style.display='none';
+        }else if(happiness<=80){
+            document.getElementById('furycat').style.display='none';
+            document.getElementById('sadcat').style.display='none';
+            document.getElementById('happycat').style.display='inline';
+            document.getElementById('happiestcat').style.display='none';
+        }else {
+            document.getElementById('furycat').style.display='none';
+            document.getElementById('sadcat').style.display='none';
+            document.getElementById('happycat').style.display='none';
+            document.getElementById('happiestcat').style.display='inline';
+        }
+        console.log(happiness);
+    },
+    1000
+);
+
