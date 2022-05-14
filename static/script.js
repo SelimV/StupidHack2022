0327ae3da9ed83e1df572a9adcdcbb2158337f5f
SelimV <selim.virtanen@gmail.com>
2022-05-14 15:04:00 +0300

    pushpush

--- static/script.js
+++ static/script.js
@@ -5,3 +5,13 @@ function eat() {
     })
     console.log("Ran ajax")
 }
+
+
+var ctx = document.getElementById('cat').getContext('2d');
+// load image
+var image = new Image();
+image.onload = function () {
+    // draw the image into the canvas
+    ctx.drawImage(image, 0, 0);
+}
+image.src = '/cats/furycat.jpg';
